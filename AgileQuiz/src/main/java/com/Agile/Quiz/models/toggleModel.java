c29f89a94675c0d644e31dfa9ef19d0dff5a409c
seans <seans@Sean-Laptop>
2017-02-22 14:38:16 +0000

    Made servlet and model for toggling availability

---
+++ AgileQuiz/src/main/java/com/Agile/Quiz/models/toggleModel.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Agile.Quiz.models;
+
+/**
+ *
+ * @author seans
+ */
+public class toggleModel {
+    
+    public void insertToDB(){}
+    
+}
