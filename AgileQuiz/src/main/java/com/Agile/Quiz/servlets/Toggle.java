43d3b34cee6511e2440069ce294fed3fa0a3f498
seans <seans@Sean-Laptop>
2017-02-23 11:17:42 +0000

    Edited Servlets -> Removed Process Request from LogOut and Toggle -> Added logout functionality. Need to know the session variables before it will fully work though.

--- AgileQuiz/src/main/java/com/Agile/Quiz/servlets/Toggle.java
+++ AgileQuiz/src/main/java/com/Agile/Quiz/servlets/Toggle.java
@@ -30,22 +30,7 @@ public class Toggle extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet toggleServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet toggleServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
+
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -59,7 +44,7 @@ public class Toggle extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+       // processRequest(request, response);
     }
 
     /**
