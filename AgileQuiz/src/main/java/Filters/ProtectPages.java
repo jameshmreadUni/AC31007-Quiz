ced3b5cb0a3b1c9622e3c883c4b784ce37b5218f
jameshmreadUni <jhmread@dundee.ac.uk>
2017-02-24 12:28:46 +0000

    Revert "Refactoring Done on File Names"

--- AgileQuiz/src/main/java/Filters/ProtectPages.java
+++ AgileQuiz/src/main/java/Filters/ProtectPages.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.Agile.Quiz.filters;
+package Filters;
 
 import com.Agile.Quiz.stores.loginBean;
 import java.io.IOException;
