453e668ff34a4e65917557cf8f916e88c88854c8
Ramesh Khairandish <r.khairandish@dundee.ac.uk>
2017-03-19 21:13:22 +0000

    Updated to Current master, everything works except database connections as i cannot connect to vpn

--- AgileQuiz/src/test/java/com/Agile/Quiz/models/ModelQuizOrderTest.java
+++ AgileQuiz/src/test/java/com/Agile/Quiz/models/ModelQuizOrderTest.java
@@ -64,7 +64,7 @@ public class ModelQuizOrderTest {
         // TODO review the generated test code and remove the default call to fail.
     }
     
-}
+
 
 /*
 
@@ -82,5 +82,5 @@ public class ModelQuizOrderTest {
         // TODO review the generated test code and remove the default call to fail.
       //  fail("The test case is a prototype.");
     }
-    
 }
+
